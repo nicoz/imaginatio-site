16c9eee32c46a09fa0be0a601a37100880b0abb5
Nicolas Zuasti <nicozuasti@gmail.com>
2014-03-07 16:28:34 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImaginatioSite::Application.config.secret_key_base = 'c71dc8dcd7fe2b4b9eebabcd0f5b3fe2a36b6d0e63b9eb401e8427fb50c47928233939aa0bac7d4a1a5f75c01084132db998850967f6f6a2cf7128273cc2204c'
